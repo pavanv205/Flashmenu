b8a0d31adbebf1fb464ded568edff19ee11ca75e
Pavan <pavanv205@gmail.com>
2026-08-18 12:09:35 +0530

    Upload local D:\egg.jpg to Cloudinary CDN and assign to Egg Curry dish item

--- server/src/utils/defaultMenu.js
+++ server/src/utils/defaultMenu.js
@@ -948,7 +948,7 @@ const defaultCategories = [
         price: 210,
         vegType: 'egg',
         subCategory: '🍗 NON VEG MAIN COURSE',
-        image: 'https://images.unsplash.com/photo-1588166524941-3bf61a9c41db?auto=format&fit=crop&w=600&q=80',
+        image: 'https://res.cloudinary.com/xt6ci0uh/image/upload/v1787035144/flashmenu/defaults/rs6cmpgfpdhrmnec2k4s.jpg',
       },
       {
         name: 'Butter Chicken Grand Style',

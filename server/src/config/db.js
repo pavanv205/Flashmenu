f3195b0329db0299edcaea8b0ec1244959c46cbd
Pavan <pavanv205@gmail.com>
2026-08-22 10:44:05 +0530

    Restrict getIsConnected to readyState === 1 to eliminate serverless buffering crashes

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -46,7 +46,7 @@ const connectDB = async () => {
 };
 
 const getIsConnected = () => {
-  return mongoose.connection.readyState === 1 || mongoose.connection.readyState === 2;
+  return mongoose.connection.readyState === 1;
 };
 
 module.exports = { connectDB, getIsConnected };
